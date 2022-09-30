05f161d497e6ff5df8bcc2275ffaaf8ccd530d5b
Сергей Ефремов <sergey_e_crazy@mail.ru>
2022-09-30 19:51:17 +0500

    test005

---
+++ test005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "даша")
+{
+    Console.WriteLine("Это моя ЛЮБИМКА");
+}
+else
+{
+    Console.Write("Hey, ");
+    Console.WriteLine(username);
+}
