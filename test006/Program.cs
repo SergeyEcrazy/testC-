e1226e861ded18bea085a241117f8476ca09d6af
Сергей Ефремов <sergey_e_crazy@mail.ru>
2022-09-30 20:10:10 +0500

    test006

--- test006/Program.cs
+++ test006/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 1;
+int b = 3;
+int c = 5;
+int d = 7;
+int e = 9;
+int max = a;
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+if (d > max ) max = d;
+if (e > max ) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
