ebd0680abadf39811193a41d1f8cddf3e00683f9
Сергей Ефремов <sergey_e_crazy@mail.ru>
2022-10-11 18:38:16 +0500

    HomeWorkSeminar2-2

---
+++ HomeWorkSeminar2-2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. Не использовать строки для расчета.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98 
+
+int random = new Random().Next(-999, 1000);
+
+int FilNumber(int numb)
+{
+    System.Console.Write($"Случайное трехзначное число: {random} ");
+    return numb;
+}
+int DelNumber(int N)
+{
+    int n1 = (N % 1000) / 100;
+    int n2 = random % 10;
+    if (N > 100 && N < 1000) System.Console.Write($"-> {n1}{n2}");
+    else
+    {
+    if (N > -999 && N < -99) System.Console.Write($"-> {n1}{n2 * -1}");
+    else
+    {
+        System.Console.Write("- число не трехзначое!");
+    }
+    }
+
+    return N;
+}
+int result = (DelNumber(FilNumber(random)));
+
