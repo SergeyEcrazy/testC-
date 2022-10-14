a9456e06a6384ad7346261e1d0d0bc39b00f9139
Сергей Ефремов <sergey_e_crazy@mail.ru>
2022-10-14 16:31:50 +0500

    HomeWorkSeminar3-3

---
+++ HomeWorkSeminar3-3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125 
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int N = Convert.ToInt32(System.Console.ReadLine());
+    return N;
+}
+int N = Prompt("Введите любое число: ");
+
+for (int i = 1; i <= N; i++)
+{
+    System.Console.WriteLine(Math.Pow(i, 3));
+}
