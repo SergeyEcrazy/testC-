4ead8bc1b2379b3278fe595c462a997043b4d6a3
Сергей Ефремов <sergey_e_crazy@mail.ru>
2022-10-21 21:30:31 +0500

    New HomeWork5-1

---
+++ HomeWork5-1/Program.cs
@@ -0,0 +1,48 @@
+﻿/*  Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+int[] CreateArray(int len, int minLimit, int maxLimit)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minLimit, maxLimit + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+void EvenNumbers(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    if (count == 0)
+    {
+        System.Console.WriteLine("Четных чисел нет");
+    }
+    else
+    {
+        System.Console.WriteLine($"Количество четных чисел >>>> {count}");
+    }
+}
+
+int[] myArray = CreateArray(2, 100, 999);
+PrintArray(myArray);
+EvenNumbers(myArray);
